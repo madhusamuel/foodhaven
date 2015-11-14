a3923fb3840d1ac66831350cfc92f95cdd3d46bb
Madhu Samuel <madhusamuel@gmail.com>
2015-11-15 09:00:22 +1100

    Added GMPMapView

--- foodhaven/FoodShareMapViewViewController.swift
+++ foodhaven/FoodShareMapViewViewController.swift
@@ -7,11 +7,53 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class FoodShareMapViewViewController: UIViewController {
+class FoodShareMapViewViewController: UIViewController, GMSMapViewDelegate {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    //TODO change to current location co-ordinates
+    let latitudeOfMelbourne = -37.8136
+    let longitudeOfMelbourne = 144.9631
+    
+    var foodShareLocations: [HomeRestaurant] = []
+    
+    var currentMarker: GMSMarker?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupMap()
+    }
+    
+    // MARK: - Setup methods
+    
+    private func setupMap() {
+        let camera = GMSCameraPosition.cameraWithLatitude(latitudeOfMelbourne,
+            longitude: longitudeOfMelbourne, zoom: 12)
+        mapView.camera = camera
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        mapView.delegate = self
+    }
+    
+    // MARK: - GMSMapViewDelegate
+    
+    func mapView(mapView: GMSMapView!, didTapInfoWindowOfMarker marker: GMSMarker!) {
+//        print("tapped me at \(marker.position)")
+//        if (UIApplication.sharedApplication().canOpenURL(NSURL(string:"comgooglemaps://")!)) {
+//            UIApplication.sharedApplication().openURL(NSURL(string:
+//                "comgooglemapsurl://?&daddr=\(marker.position.latitude),\(marker.position.longitude)&directionsmode=walking")!)
+//        } else {
+//            print("Can't use comgooglemaps://");
+//            let alertController = UIAlertController(title: nil, message: "Unable to open google maps! You need to run this app in an iOS 9 device.", preferredStyle: .Alert)
+//            let okAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+//                alertController.dismissViewControllerAnimated(false, completion: nil)
+//            }
+//            alertController.addAction(okAction)
+//            presentViewController(alertController, animated: true, completion: nil)
+//            
+//        }
     }
 
 }
