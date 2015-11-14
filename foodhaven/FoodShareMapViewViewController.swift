6816947707af37ae4fb3c74242d9747b26c05635
Madhu Samuel <madhusamuel@gmail.com>
2015-11-15 08:42:58 +1100

    Created mapviewcontroller.

---
+++ foodhaven/FoodShareMapViewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FoodShareMapViewViewController.swift
+//  foodhaven
+//
+//  Created by Madhu Samuel on 14/11/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class FoodShareMapViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
