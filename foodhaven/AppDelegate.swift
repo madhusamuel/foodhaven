90294768eeb456f153b54b95903025fc8ad150b9
Chris Hatton <chris@bluedot.com.au>
2015-11-15 13:39:59 +1100

    Basic integration complete

--- foodhaven/AppDelegate.swift
+++ foodhaven/AppDelegate.swift
@@ -10,38 +10,103 @@ import UIKit
 import GoogleMaps
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+public class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    public var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    public func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyCpR0H-3o6sRwsIAjW-6nw7XOYIVyo3Uf0")
+        
+        let locationManager = BDLocationManager.instance()
+        
+        //  Assign the delegates to this class
+        locationManager.locationDelegate = self
+        locationManager.sessionDelegate  = self
+        
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    public func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    public func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    public func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    public func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    public func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+
+}
+
+extension AppDelegate : BDPSessionDelegate {
+    
+    public func willAuthenticateWithUsername( username: String!, apiKey: String!, packageName: String!) {
+
+    }
+
+    public func authenticationWasSuccessful() {
+        
+    }
+
+    public func authenticationWasDeniedWithReason( reason: String!) {
+        
+    }
+
+    public func authenticationFailedWithError( error: NSError!) {
+        
+    }
+
+    public func didEndSession() {
+        
+    }
+}
+
+extension AppDelegate : BDPLocationDelegate {
+    
+    public func didUpdateZoneInfo(zoneInfos: Set<NSObject>!) {
+        
+    }
+
+    public func didCheckIntoFence( fence: BDFenceInfo?, inZone zoneInfo: BDZoneInfo?, atCoordinate coordinate: BDLocationCoordinate2D, onDate date: NSDate!) {
+        
+        // TODO: Implement here: To show 
+        
+    }
+
+    public func didCheckIntoBeacon( beacon: BDBeaconInfo?, inZone zoneInfo: BDZoneInfo?, withProximity proximity: CLProximity, onDate date: NSDate!) {
+        
+    }
+
+    public func didStartRequiringUserInterventionForBluetooth() {
+    
+    }
+
+    public func didStopRequiringUserInterventionForBluetooth() {
+    
+    }
+
+    public func didStartRequiringUserInterventionForLocationServices() {
+    
+    }
+
+    public func didStopRequiringUserInterventionForLocationServices() {
+    
+    }
 
 }
 
