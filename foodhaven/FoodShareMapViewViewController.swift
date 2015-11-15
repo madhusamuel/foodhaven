c67ed030a54e3a314c95e160be6506c3c18d4d4d
Madhu Samuel <madhusamuel@gmail.com>
2015-11-15 13:03:08 +1100

    images integrated

--- foodhaven/FoodShareMapViewViewController.swift
+++ foodhaven/FoodShareMapViewViewController.swift
@@ -36,7 +36,7 @@ class FoodShareMapViewViewController: UIViewController, GMSMapViewDelegate {
     
     private func setupMap() {
         let camera = GMSCameraPosition.cameraWithLatitude(latitudeOfMelbourne,
-            longitude: longitudeOfMelbourne, zoom: 15)
+            longitude: longitudeOfMelbourne, zoom: 14)
         mapView.camera = camera
         mapView.myLocationEnabled = true
         self.view = mapView
@@ -94,7 +94,6 @@ class FoodShareMapViewViewController: UIViewController, GMSMapViewDelegate {
         let rest1 = HomeRestaurant(name: "Barbara's Kitchen", location: CLLocationCoordinate2D(latitude: -37.598473, longitude: 144.221200), specialFoodItem: foodItems[0])
         
         rest1.menu = Menu()
-        rest1.menu.photo = "Kitchen1.jpg"
         rest1.menu.foodItems = foodItems
         return [rest1]
     }
@@ -103,21 +102,39 @@ class FoodShareMapViewViewController: UIViewController, GMSMapViewDelegate {
         return [
             createRestaurant("Barbara's Kitchen", location: CLLocationCoordinate2D(latitude: -37.598473, longitude: 144.221200),
                 foodItems:
-                    [FoodItem(name: "Pizza", price: 10, stock: 5, ingredients: "Flour", photo: "pizza.jpg"),
-                    FoodItem(name: "Pizza", price: 10, stock: 5, ingredients: "Flour", photo: "pizza.jpg")],
-                photo: "kitchen1.jpg"),
-            createRestaurant("Barbara's Kitchen", location: CLLocationCoordinate2D(latitude: -37.601637, longitude: 144.223335),
+                [FoodItem(name: "Beef Lasangna", price: 10, stock: 5, ingredients: "Beef, Plain Flour, Egg, cream, garlic, onion, Olive Oil, pasta sause, red wine, cheddar cheese, parmesan cheese", photo: "beef_lasagna"),
+                    FoodItem(name: "Chocolate Chip Cookie", price: 2, stock: 30, ingredients: "Plain flour, egg, butter, sugar, chocolate chips", photo: "chocolate_chip_cookie")],
+                photo: "elderly_women_in_kitchen"),
+            createRestaurant("Sarah's Kitchen", location: CLLocationCoordinate2D(latitude: -37.601637, longitude: 144.223335),
                 foodItems:
-                [FoodItem(name: "Pizza", price: 10, stock: 5, ingredients: "Flour", photo: "pizza.jpg"),
-                    FoodItem(name: "Pizza", price: 10, stock: 5, ingredients: "Flour", photo: "pizza.jpg")],
-                photo: "kitchen1.jpg")
+                [FoodItem(name: "Pizza(Veg)", price: 13, stock: 2, ingredients: "Silverbeet, broccolini and mozzarella pizza", photo: "pizza2"),
+                    FoodItem(name: "Pizza(Non-veg)", price: 13, stock: 2, ingredients: "Chicken, mushroom and mozzarella pizza", photo: "pizza1"),
+                    FoodItem(name: "Meat Loaf", price: 10, stock: 5, ingredients: "veal mince, garlic, onion, breadcrumbs, egg, worcestershire sause, steamed green beans and gravy to serve", photo: "meat_loaf")],
+                photo: "US_family_in_kitchen"),
+            createRestaurant("Spicy Kitchen", location: CLLocationCoordinate2D(latitude: -37.603344, longitude: 144.223346),
+                foodItems:
+                [FoodItem(name: "Indian bread(Roti)", price: 10, stock: 5, ingredients: "Wheat Flour, salt, olive oil", photo: "chapati"),
+                    FoodItem(name: "Spicy Goat Curry", price: 2, stock: 30, ingredients: "Goat, ginger, garlic, onion, tomato, garam masala, chilli, curry leaves, coconut oil", photo: "goat_curry")],
+                photo: "family_in_kitchen"),
+            createRestaurant("Eva's Kitchen", location: CLLocationCoordinate2D(latitude: -37.602282, longitude: 144.231143),
+                foodItems:
+                [FoodItem(name: "Butter Dosa with Potato Masala", price: 10, stock: 5, ingredients: "Rice, Urud Dhal, Potato, chilli, tomato, turmeric, ginger", photo: "masala_dosa")],
+                photo: "granny_kid_in_kitchen"),
+            createRestaurant("Mia's Kitchen", location: CLLocationCoordinate2D(latitude: -37.602169, longitude: 144.233822),
+                foodItems:
+                [FoodItem(name: "Pasta", price: 11, stock: 4, ingredients: "Wheat Flour, salt, olive oil", photo: "pasta")],
+                photo: "old_man_in_kitchen"),
+            createRestaurant("Toms", location: CLLocationCoordinate2D(latitude: -37.600729, longitude: 144.236548),
+                foodItems:
+                [FoodItem(name: "Pasta", price: 11, stock: 4, ingredients: "Wheat Flour, salt, olive oil", photo: "pasta")],
+                photo: "man_in_kitchen")
         ]
     }
     
     func createRestaurant(name: String, location: CLLocationCoordinate2D, foodItems: [FoodItem], photo: String) -> HomeRestaurant {
         let homeRestaurant = HomeRestaurant(name: name, location: location, specialFoodItem: foodItems[0])
+        homeRestaurant.photoName = photo
         homeRestaurant.menu = Menu()
-        homeRestaurant.menu.photo = photo
         homeRestaurant.menu.foodItems = foodItems
         return homeRestaurant
     }
