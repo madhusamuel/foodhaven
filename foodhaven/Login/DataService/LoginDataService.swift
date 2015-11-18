2e568c6e0464f1531259272d752d8ec50c839ed2
Madhu Samuel <madhusamuel@gmail.com>
2015-11-18 16:38:32 +1100

    Added LoginDataService.

---
+++ foodhaven/Login/DataService/LoginDataService.swift
@@ -0,0 +1,13 @@
+//
+//  LoginDataService.swift
+//  foodhaven
+//
+//  Created by Madhu Samuel on 18/11/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import Foundation
+
+class LoginDataService {
+    
+}
