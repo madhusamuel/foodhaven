d8d4056ba06fa154fc7e1b95f2c4f1852fa1feb8
Madhu Samuel <madhusamuel@gmail.com>
2015-11-18 15:30:58 +1100

    Added LoginTableViewController

---
+++ foodhaven/Login/LoginTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginTableViewController.swift
+//  foodhaven
+//
+//  Created by Madhu Samuel on 18/11/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
