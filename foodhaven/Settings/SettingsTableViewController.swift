0db261f940bd58adbf69a3a27b6ab5b4edf58937
Madhu Samuel <madhusamuel@gmail.com>
2015-11-18 15:29:25 +1100

    Added SettingsTableViewController.

---
+++ foodhaven/Settings/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  foodhaven
+//
+//  Created by Madhu Samuel on 18/11/2015.
+//  Copyright © 2015 Madhu. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+}
